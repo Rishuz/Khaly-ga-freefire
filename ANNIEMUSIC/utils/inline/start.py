45ea6b079741ed192e62c57109f6f0ef6b6e9ee6
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-20 20:04:39 +0530

    Update start.py

--- ANNIEMUSIC/utils/inline/start.py
+++ ANNIEMUSIC/utils/inline/start.py
@@ -30,6 +30,8 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+        ],[
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settingts_back_helper"),
         ],
     ]
     return buttons
